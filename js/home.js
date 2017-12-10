9dca928789bb628a7ea93a02fcf62d56b8abc838
Marc Pittinsky <marclane@live.unc.edu>
2017-12-10 02:15:11 -0500

    spacing

--- js/home.js
+++ js/home.js
@@ -1,13 +1,13 @@
 $( document ).ready(function() {
 	mdc.autoInit();
 	$("#overlay").css("display","block");
-			$.ajax({
-			  url: "http://localhost:8000/php/questions.php/recent-activity/"+$.cookie("uid"),
-			  method: "GET",
-			  dataType: "json"
-			}).done(function(response) {
-				$("#overlay").css("display","none");
-			}).fail(function(error) {
-				console.log(error); //log them out or something
-			});
+	$.ajax({
+	  url: "http://localhost:8000/php/questions.php/recent-activity/"+$.cookie("uid"),
+	  method: "GET",
+	  dataType: "json"
+	}).done(function(response) {
+		$("#overlay").css("display","none");
+	}).fail(function(error) {
+		console.log(error); //log them out or something
+	});
 });
