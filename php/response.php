306270b580c5e24e3a64e294fe0286dd1f239768
jakermayer <jakemayer8506@gmail.com>
2017-12-13 14:04:23 -0500

    Added endpoint for response data

--- php/response.php
+++ php/response.php
@@ -8,7 +8,19 @@ require_once('orm/Response.php');
 $path_components = explode('/', $_SERVER['PATH_INFO']);
 
 if ($_SERVER['REQUEST_METHOD'] == "GET") {
-	
+	if(count($path_components) >=2 && $path_components[1] != "") {
+		$is_authorized = true;//Token::authorizeRequest($_COOKIE['uid'], $_COOKIE['uuid']);
+		if($is_authorized) {
+			header("Content-Type: application/json");
+			print(json_encode(Response::getResponseData($path_components[1], $_GET['races'], $_GET['religions'], $_GET['countries'], $_GET['genders'], 
+														$_GET['start_date'], $_GET['end_date'])));
+			exit();
+		} else {
+			header("HTTP/1.0 401 Unauthorized");
+			print("oh no!");
+			exit();
+		}
+	}
 } else if ($_SERVER['REQUEST_METHOD'] == "POST") {
 	if(count($path_components) >=2 && $path_components[1] != "") {
 		$is_authorized = Token::authorizeRequest($_COOKIE['uid'], $_COOKIE['uuid']);
